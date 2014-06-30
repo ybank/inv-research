08cbc7849b933981c3e2aa6c2783de719ba0784c
Y. Yan <ybank.net@gmail.com>
2014-06-29 19:29:38 -0700

    YY: Initialize maven project.

---
+++ src/main/java/experiments/invariants/App.java
@@ -0,0 +1,13 @@
+package experiments.invariants;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
